e639a6204ec87e86cee89b73d1a765b8fb9b1d23
kerns <david@kerns.dk>
2022-12-31 00:39:40 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# sveltejs-kit-template-default-gjfn9e
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/sveltejs-kit-template-default-gjfn9e)
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+  import gsap from "gsap-trial";
+  import { ScrollTrigger } from "gsap-trial/dist/ScrollTrigger";
+  import { ScrollSmoother } from "gsap-trial/dist/ScrollSmoother";
+	import './styles.css';
+	
+  if (typeof window !== "undefined") {
+    gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+  }
+</script>
+
+<div class="app">
+	<main>
+		<slot />
+	</main>
+</div>
